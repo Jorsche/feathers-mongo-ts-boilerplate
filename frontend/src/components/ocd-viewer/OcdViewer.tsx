9059bad67eecef4a58f23cbc87d220f75add40d3
Jorsche <jorschetan@hotmail.com>
2021-06-17 16:16:06 +0800

    Add ocd viewer with react grid

--- frontend/src/components/ocd-viewer/OcdViewer.tsx
+++ frontend/src/components/ocd-viewer/OcdViewer.tsx
@@ -2,58 +2,121 @@ import React, {useState} from "react";
 import {Button, Dropdown} from 'semantic-ui-react'
 import "./OcdViewer.css"
 import BoundedLayout from "../react-grid/react-grid";
+import _ from 'lodash';
+import client from '../../feathers';
 
-const OcdViewer = ()=>{
-    const [friendOptionsArray, setFriendOptionsArray] = useState([
+const OcdViewer = ({name})=>{
+    const widgetService = client.service('widgets');
+    const [widgetOptionSelection, setWidgetOptionsArray] = useState([
         {
-            key: 'Jenny Hess',
-            text: 'Jenny Hess',
-            value: 'Jenny Hess',
+            key: 'Widget1',
+            text: 'Widget1',
+            value: 'Widget1',
         },
         {
-            key: 'Elliot Fu',
-            text: 'Elliot Fu',
-            value: 'Elliot Fu',
+            key: 'Widget2',
+            text: 'Widget2',
+            value: 'Widget2',
         },
         {
-            key: 'Stevie Feliciano',
-            text: 'Stevie Feliciano',
-            value: 'Stevie Feliciano',
+            key: 'Widget3',
+            text: 'Widget3',
+            value: 'Widget3',
         },
         {
-            key: 'Christian',
-            text: 'Christian',
-            value: 'Christian',
+            key: 'Widget4',
+            text: 'Widget4',
+            value: 'Widget4',
         },
         {
-            key: 'Matt',
-            text: 'Matt',
-            value: 'Matt',
+            key: 'Widget5',
+            text: 'Widget5',
+            value: 'Widget5',
         },
         {
-            key: 'Justen Kitsune',
-            text: 'Justen Kitsune',
-            value: 'Justen Kitsune',
+            key: 'Widget6',
+            text: 'Widget6',
+            value: 'Widget6',
         },
     ]);
-    const [dropdownValueArray, setDropdownValueArray] = useState([]);
-    const [dropdownValue, setDropdownValue] = useState("");
     const [itemState, setItemState] = useState({
+        layout:[],
         items:[],
         newCounter: 0
     });
+    const [saveClick, setSaveClick] = useState(false);
+    const [resState, setResState] = useState({});
     const availableHandles = ["s", "w", "e", "n", "sw", "nw", "se"];
-    {/*button appears only when it detects shifting of position OR new widgets are added  */}
-    //new widgets added can use to cmp
-    // const saveButtonDisplay =
+
+    // widgetService.aggregate([{
+    //     $lookup: {
+    //         from: 'User',
+    //         localField: 'User_ID',
+    //         foreignField: 'ID',
+    //         as: 'Customer'
+    //     }}]);
+    //
+
+
+    const retrievalOfItems =async ()=>{
+        try {
+           const response =await widgetService.find();
+            if(response.data.length>0) {
+                const fetchData = response.data[0];
+                setResState(fetchData);
+                setItemState(fetchData);
+                const results = widgetOptionSelection.filter(({ value: id1 }) => !fetchData.layout.some(({ i: id2 }) => id2 === id1));
+                setWidgetOptionsArray(results);
+            }
+        }
+        catch(e){
+            console.log("error",e);
+        }
+
+    }
+    console.log("resState",resState);
+
+    const saveOrEdit = async (itemState,resState)=>{
+        try {
+            console.log("!_.isEmpty(resState)",!_.isEmpty(resState));
+            if(_.isEmpty(resState)){
+                widgetService.create(itemState).then(()=>{
+                    retrievalOfItems();
+                })
+            }
+            else{
+                widgetService.patch(itemState._id,itemState).then(()=>{
+                    retrievalOfItems();
+                })
+            }
+        } catch (error) {
+            console.log("err", error);
+        }
+
+        // const postOrPut= _.isEmpty(resState)?axios.post:axios.patch;
+        // try {
+        //     postOrPut(`http://localhost:3030/widgets/${!_.isEmpty(resState) ? itemState._id: ""}`, itemState).then(()=>{
+        //         retrievalOfItems();
+        //     })
+        // } catch (error) {
+        //         console.log("err", error);
+        //     }
+
+    }
     const onChange = (e: any, {value}: any) => {
-        setDropdownValueArray(dropdownValueArray.concat(value));
-        setFriendOptionsArray(friendOptionsArray.filter((val)=>(
+        setWidgetOptionsArray(widgetOptionSelection.filter((val)=>(
             val.value!==value
         )));
-        setDropdownValue(value);
-            // /*eslint no-console: 0*/
         setItemState({
+            ...itemState,
+                layout: itemState.layout.concat({
+                    i: value,
+                    x: (itemState.items.length * 2) % (itemState.cols || 12),
+                    y: Infinity, // puts it at the bottom
+                    w: 2,
+                    h: 2,
+                    resizeHandles: availableHandles
+                }),
                 items: itemState.items.concat({
                     i: value,
                     x: (itemState.items.length * 2) % (itemState.cols || 12),
@@ -66,39 +129,38 @@ const OcdViewer = ()=>{
                 newCounter: itemState.newCounter + 1
             });
     };
-    console.log("itemState",itemState);
-    const DropdownExampleSelection = () => (
+    const DropdownSelection = () => (
         <Dropdown
             placeholder='Add Widget'
             selection
-            options={friendOptionsArray}
+            options={widgetOptionSelection}
             onChange={onChange}
         />
     )
-    console.log("dropdownValue",dropdownValue);
     return(
     <div className="ocd-viewer">
-    <div className="ocd-viewer-header">header</div>
+    <div className="ocd-viewer-header">{name}header</div>
     <div className="ocd-viewer-main">
-        <BoundedLayout itemState={itemState} friendOptionsArray={friendOptionsArray} setFriendOptionsArray={setFriendOptionsArray}setItemState={setItemState}></BoundedLayout>
-       {/*<TestHook dropDownOption={dropdownValue}></TestHook>*/}
-
-        {/*{dropdownValueArray.map((val)=> {*/}
-        {/*  return <Widget*/}
-        {/*      name={val}*/}
-        {/*      setFriendOptionsArray={setFriendOptionsArray}*/}
-        {/*      friendOptionsArray={friendOptionsArray}*/}
-        {/*      setDropdown= {setDropdownValueArray}*/}
-        {/*      dropdownArr={dropdownValueArray}*/}
-        {/*  />*/}
-        {/*})*/}
-        {/*}*/}
+        <BoundedLayout
+            retrievalOfItems={retrievalOfItems}
+            itemState={itemState}
+            widgetOptionSelection={widgetOptionSelection}
+            setWidgetOptionsArray={setWidgetOptionsArray}
+            setItemState={setItemState}
+            saveClick={saveClick}
+        >
+        </BoundedLayout>
     </div>
     <div className="ocd-viewer-footer">
         footer
-   <DropdownExampleSelection></DropdownExampleSelection>
-        {/*button appears only when it detects shifting of position OR new widgets are added  */}
-        <Button>Save & Published</Button>
+   <DropdownSelection></DropdownSelection>
+        <Button
+            className="add-widget-button"
+            disabled={JSON.stringify(itemState) === JSON.stringify(resState)}
+            onClick={
+                ()=>{saveOrEdit(itemState,resState)
+            setSaveClick(!saveClick)
+        }}>Save & Publish</Button>
     </div>
 </div>
 )
