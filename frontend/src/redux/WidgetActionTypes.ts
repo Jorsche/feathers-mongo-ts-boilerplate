fca6c5875c97896a1400aee47d311a7f5a8ea5f8
Jorsche <jorschetan@hotmail.com>
2021-07-04 18:33:58 +0800

    Remove usage of ducks

--- frontend/src/redux/WidgetActionTypes.ts
+++
@@ -1,1 +0,0 @@
-export const REDUXVAR = 'REDUXVAR';
--- frontend/src/redux/WidgetActions.ts
+++
@@ -1,6 +0,0 @@
-import * as WidgetActionTypes from './WidgetActionTypes';
-
-export const setReduxVar = (aVar: number) => ({
-  type: WidgetActionTypes.REDUXVAR,
-  reduxVar: aVar,
-});
